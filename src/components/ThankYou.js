98c293ef88f4d44b51ed01d5b825dc75761de515
MuhammadGb <49286121+MuhammadGb@users.noreply.github.com>
2021-09-28 11:56:40 +0100

    Update ThankYou.js

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
-import JobDetails from './components/JobDetails';
-import ThankYou from './components/ThankYou';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
 
 export default function App() {
-
   return (
-    <div style={{fontSize: "6em"}}>Thank You For Applying With Us</div>
+    <div style={{ fontSize: '5em', marginLeft: '1em' }}>
+      Thank You For Applying With Us
+    </div>
   );
 }
-
